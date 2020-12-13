186bcdd4e59a9c52fc5315a353650728b3e83a49
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-12-13 15:18:25 +0000

    terratest for azurerm_network fix #patch

--- examples/azurerm_network/main.tf
+++ examples/azurerm_network/main.tf
@@ -2,25 +2,19 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_string" "salt" {
-  length  = 4
-  special = false
-  upper   = false
-}
-
 module "networking" {
   source = "../../modules/azurerm_network"
 
-  rg_name  = "base-net-rg-${random_string.salt.result}"
+  rg_name  = "base-net-rg"
   location = "North Europe"
 
   custom_tags = {
     Environment = "demo"
   }
 
-  ddos_protection_plan_name = "ddospplan-${random_string.salt.result}"
+  ddos_protection_plan_name = "ddospplan"
 
-  vnet_name     = "base-vnet-${random_string.salt.result}"
+  vnet_name     = "base-vnet"
   address_space = "10.0.0.0/16"
 
   subnets = {
--- test/azurerm_network_test.go
+++ test/azurerm_network_test.go
@@ -3,26 +3,19 @@ package test
 import (
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/azure"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestAzureNetworkExample(t *testing.T) {
 	t.Parallel()
 
 	// subscriptionID is overridden by the environment variable "ARM_SUBSCRIPTION_ID"
-	subscriptionID := ""
-	salt := random.UniqueId()
+	//subscriptionID := ""
 
 	// website::tag::1:: Configure Terraform setting up a path to Terraform code.
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
 		TerraformDir: "../examples/azurerm_network",
-		Vars: map[string]interface{}{
-			"rg_name": "base-net-rg-" + salt,
-		},
 	}
 
 	// website::tag::4:: At the end of the test, run `terraform destroy` to clean up any resources that were created
